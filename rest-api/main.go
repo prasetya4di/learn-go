b24ffd24a8959125cbe973c18ecf6190c9199213
Pras <prasetyaarfanto@gmail.com>
2023-02-08 20:22:54 +0700

    Add api get albums

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"web-service-gin/model"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", model.GetAlbums)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- rest-api/model/album.go
+++ rest-api/model/album.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -12,4 +17,9 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// GetAlbums responds with the list of all albums as JSON.
+func GetAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
