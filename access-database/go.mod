7c0e767097f34caa8b1284b87133a79e608b3ea3
Pras <prasetyaarfanto@gmail.com>
2023-02-08 22:18:17 +0700

    Add access database module

---
+++ access-database/go.mod
@@ -0,0 +1,7 @@
+module data-access
+
+go 1.20
+
+require github.com/go-sql-driver/mysql v1.7.0
+
+require github.com/joho/godotenv v1.5.1
---
+++ access-database/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+var db *sql.DB
+
+func init() {
+	// Get environment data
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	// Capture connection properties
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected !!")
+}
