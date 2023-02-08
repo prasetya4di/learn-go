0c1ff35074a08960f6723080c3a3def611f407c1
Pras <prasetyaarfanto@gmail.com>
2023-02-08 19:56:30 +0700

    Add new function to generate greetings for slice of names

--- hello/hello.go
+++ hello/hello.go
@@ -17,15 +17,19 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names.
+	names := []string{"Pras", "Adi", "Prasaja"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("You")
+	messages, err := greetings.Hellos(names)
+
 	// If an error was returned, print it to the console and
 	// exit the program
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned mesage
-	// to the console
-	fmt.Println(message)
+	// If no error was returned, print the returned map
+	// of messages to the console
+	fmt.Println(messages)
 }
